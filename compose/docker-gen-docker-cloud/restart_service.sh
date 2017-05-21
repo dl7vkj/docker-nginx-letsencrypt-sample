7ef5be8926535739fb9f977209e4b3f926a8c2e0
Daniel Heydemann <dl7vkj@gmail.com>
2017-05-21 18:24:18 +0200

    Modify restart_service.sh for docker swarm

--- compose/docker-gen-docker-cloud/restart_service.sh
+++ compose/docker-gen-docker-cloud/restart_service.sh
@@ -2,7 +2,8 @@
 
 PROXY_SERVICE=$PROXY_SERVICE_ENV_VAR
 
-echo "Redeploying proxy service [${NGINX_PROXY_SERVICE}]..."
-proxy=`docker service ps --status Running | grep "^${PROXY_SERVICE}" | awk '{print $2}'`
-docker service redeploy $proxy
-echo "Redeployed proxy service [${NGINX_PROXY_SERVICE}]"
+echo "Redeploying proxy service [${PROXY_SERVICE}]..."
+# proxy=`docker service ps --status Running | grep "^${PROXY_SERVICE}" | awk '{print $2}'`
+# docker service redeploy $proxy
+docker service update ${PROXY_SERVICE} --force
+echo "Redeployed proxy service [${PROXY_SERVICE}]"
